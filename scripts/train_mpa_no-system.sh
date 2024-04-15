c0e0dcd79344e206924b3079e39100c257fec560
suehyunpark <shparksue@gmail.com>
2024-04-15 16:30:42 +0000

    Add scripts and config for ablation

--- scripts/train_mpa_no-system.sh
+++ scripts/train_mpa_no-system.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --job-name=mpa_mistral-7b-default-system        # Job name
+#SBATCH --job-name=mpa_mistral-7b-no-system        # Job name
 #SBATCH -o /mnt/nas/suehyun/axolotl/logs/out_%x.txt              # Path to output log file (%j expands to job name)
 #SBATCH -e /mnt/nas/suehyun/axolotl/logs/err_%x.err              # Path to error log file (%j expands to job name)
 #SBATCH --partition=LocalQ         # Partition name
@@ -14,4 +14,4 @@ export WANDB_API_KEY="339cad8697ca8b7558010d3f8c4aa40788e64d12"
 export HF_TOKEN="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"
 
 # echo "Starting job"
-srun accelerate launch --main_process_port 0 -m axolotl.cli.train /mnt/nas/suehyun/axolotl/examples/mpa/default-system/mistral-7b.yml
+srun accelerate launch --main_process_port 0 -m axolotl.cli.train /mnt/nas/suehyun/axolotl/examples/mpa/no-system/mistral-7b.yml
